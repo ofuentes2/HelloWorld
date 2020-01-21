5659b05b434358502263ea24fd4e81f3bd313d94
elect <ofuentes2@mail.usf.edu>
2020-01-21 17:20:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Programmer: Omar Fuentes
+// Project: Lab 2-1
+// Date: 01/21/2020
+// Description: Welcome to Lab 2-1
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display Hello World!
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
